ac4adbbfec85455b7efef3a56eed7245e3b1a3ad
Arafat <mdarafat3167@gmail.com>
2025-09-29 02:12:43 +0600

    complete design the card section

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,4 +1,6 @@
 import React, { use } from 'react';
+import userImg from '../../assets/user.png';
+import flagImg from '../../assets/flag.png';
 
 const AvailablePlayers = ({ playerPromise }) => {
   const playerData = use(playerPromise);
@@ -6,7 +8,59 @@ const AvailablePlayers = ({ playerPromise }) => {
 
   return (
     <>
-      <p>Available</p>
+      <div className="max-w-[1200px] mx-auto grid md:grid-cols-3 gap-5">
+        {playerData.map(player => (
+          <div className="card bg-base-100 shadow-sm p-4">
+            <figure>
+              <img
+                src={player.Player_image}
+                alt="Shoes"
+                className="w-full h-[300px] object-cover"
+              />
+            </figure>
+            <div className="mt-4">
+              <div className="flex gap-2">
+                <img src={userImg} alt="" />
+                <h2 className="card-title">{player.Player_name}</h2>
+              </div>
+
+              <div className="flex justify-between items-center border-b-1 border-gray-300 pb-2">
+                <div className="flex items-center">
+                  <img className="w-[20px] h-[20px]" src={flagImg} alt="" />
+                  <span className="ml-3">{player.player_country}</span>
+                </div>
+                <button className="btn">{player.playing_role}</button>
+              </div>
+
+              <div className="flex justify-between items-center my-2">
+                <span className="font-bold">Rating</span>
+                <span>{player.Rating}</span>
+              </div>
+
+              <div className="flex justify-between items-center my-2">
+                <span className="font-bold">Batting Style</span>
+                <span className="text-gray-400 font-semibold">
+                  {player.Batting_style}
+                </span>
+              </div>
+
+              <div className="flex justify-between items-center my-2">
+                <span className="font-bold">Bowling Style</span>
+                <span className="text-gray-400 font-semibold">
+                  {player.Bowling_style}
+                </span>
+              </div>
+
+              <div className="flex justify-between items-center my-2">
+                <p className="font-bold">Price: ${player.price}</p>
+                <button className="btn text-gray-400 border-1 border-gray-100">
+                  Choose Player
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
